d67f44379eda3586aad2a35f9bc56468becc6e48
martin-mann <qyu@gmx.de>
2017-05-29 13:40:51 +0200

    version set to 2.0.0

--- src/de/uni_freiburg/bioinf/mica/controller/MicaController.java
+++ src/de/uni_freiburg/bioinf/mica/controller/MicaController.java
@@ -8,7 +8,7 @@ public interface MicaController {
 	/**
 	 * Version information
 	 */
-	public static final String version = "2.0.0-trunk";
+	public static final String version = "2.0.0";
 
 	/**
 	 * To describe on what basis the distance function is to be computed
